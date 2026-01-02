bd20aa66e783c07b64fe1a7838bb8ccf50a2411f
Sana Mukhtar <130250734+sana-mukhtar@users.noreply.github.com>
2026-01-02 10:52:17 +0530

    fix3

--- pastebin-frontend/app/page.tsx
+++ pastebin-frontend/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     setPasteURL("");
 
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API_URL), {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ content, max_views: maxViews, ttl_seconds: ttlSeconds }),
